c3152cb8bd25c056cb65d322782ccc13834b6891
jlee <jlee@DIVWL-515464BA.(none)>
2010-03-13 21:26:47 -0500

    Add a self contained server+test example

---
+++ examples/nodeloadlib-ex2.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+// Self contained node.js HTTP server and a load test against it. Just run:
+//
+//     node examples/nodeloadlib-ex2.js
+//
+var http = require('http');
+var sys = require('sys');
+require('../nodeloadlib');
+sys.puts("Test server on localhost:9000.");
+http.createServer(function (req, res) {
+    res.sendHeader((Math.random() < .8) ? 200 : 404, {'Content-Type': 'text/plain'});
+    res.write('foo\n');
+    res.close();
+}).listen(9000);
+
+var test = addTest({
+    name: "Read",
+    host: 'localhost',
+    port: 9000,
+    numClients: 10,
+    timeLimit: 6000,
+    targetRps: 500,
+    successCodes: [200,404],
+    reportInterval: 2,
+    stats: ['result-codes', 'latency', 'concurrency', 'uniques'],
+    latencyConf: {percentiles: [.90, .999]},
+    requestGenerator: function(client) {
+        return traceableRequest(client, 'GET', "/" + Math.floor(Math.random()*8000), { 'host': 'localhost' });
+    }
+});
+
+startTests();
