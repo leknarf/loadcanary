2d704da9da73c916729e36b600519ef514b6b08a
Andrew Frankel <andrew@leknarf.net>
2013-06-01 20:17:20 -0400

    Remove comments

--- server.js
+++ server.js
@@ -1,13 +1,6 @@
-//var coffee = require('coffee-script');
-//var app = require('./app.coffee');
-//var port = process.env.PORT || 1337
-
 var express = require('express');
 var app = express();
 app.configure(function(){
-  //server.use('/media', express.static(__dirname + '/media'));
   app.use(express.static(__dirname + '/html'));
 });
 app.listen(80);
-
-// app.application.listen(port);
