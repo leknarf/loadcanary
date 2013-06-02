408508ec900828904d7bd3f57a318824afe05987
oscargt <oscar@blubee.com>
2013-06-01 23:41:34 -0400

    changes

---
+++ doloadtest.js
@@ -0,0 +1,37 @@
+module.exports = {
+run: function() {
+   // This test will hit localhost:8080 with 20 concurrent connections for 10 minutes.
+    var http = require('http'),
+    nl = require('nodeload/nodeload'),
+	fs = require('fs');
+
+    http.createServer(function (req, res) { res.writeHead(200); res.end(); }).listen(8080);
+    console.log("Server to load test listening on 8080.");
+
+    var files = fs.readdirSync('.');
+    var removeRegex =/results.+(html|log)/;
+    for(var i = 0; i < files.length; i++) {
+	if(removeRegex.test(files[i])) {
+		//console.log(files[i]);
+		fs.unlinkSync(files[i]);
+	}
+    }
+
+    var loadtest = nl.run({
+        name: 'Example',
+        host: 'localhost',
+        port: 8000,
+        numClients: 20,
+        timeLimit: 30,
+        targetRps: 200,
+	loadProfile: [[0,0], [3, 20], [6, 200], [9, 300]],
+	userProfile: [[0,0], [3, 2], [6, 20], [9, 50]],
+        stats: ['latency', 'result-codes', { name: 'http-errors', successCodes: [200], log: 'http-errors.log' }],
+        requestGenerator: function(client) {
+            return client.request('GET', "/" + Math.floor(Math.random()*10000));
+        }
+    });
+    loadtest.on('end', function() { process.exit(0); });
+    return 'running...';
+}
+};
--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 var express = require('express');
+
 var app = express();
 app.configure(function(){
   app.use(express.static(__dirname + '/html'));
 });
-app.listen(80);
+
+app.get('/request', function(req, res){
+    var request = require('./doloadtest.js');
+    res.send(request.run()); // try res.json() if result is an object or array
+});
+app.get('/getdata', function(req, res){
+    var request = require('./monitor.js');
+    res.json(request.run()); // try res.json() if result is an object or array
+});
+
+app.listen(1337);
